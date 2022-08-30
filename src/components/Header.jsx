da0c34b81b24a5fef379a8ab92b4e1eed8a27fb5
Rodrigo Rivas <el_poca@hotmail.com>
2022-08-30 00:19:59 -0600

    Fixing tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      <h1 className=' text-4xl font-bold uppercase mt-5'>Desde header</h1>
+      <h1 className=' bg-slate-600'>Desdesadasasdsadda header</h1>
     </header>
 
   )
